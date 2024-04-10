c9b9d135b49ce3b71874643d84503b5b27de7665
Cristian Camargo <contato@camargodev.com.br>
2024-04-10 10:25:46 -0400

    feat: adiciona informatica

--- models/Categories.ts
+++ models/Categories.ts
@@ -21,6 +21,8 @@ const Categories = [
     {name: 'Câmera Digital', value: 'CÂMERA DIGITAL'},
     {name: 'Câmera de Segurança', value: 'CÂMERA DE SEGURANCA'},
     {name: 'Games', value: 'GAMES'},
+    {name: 'Games', value: 'GAMES'},
+    {name: 'Informática', value: 'INFORMATICA'},
   ]
 
 export default Categories
