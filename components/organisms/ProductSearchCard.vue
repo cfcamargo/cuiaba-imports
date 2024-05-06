86d03afc3be0a2db0826908dc071b25d63b67c6c
Cristian <contato@camargodev.com.br>
2024-05-05 22:34:04 -0300

    feat: adiciona search results

---
+++ components/organisms/ProductSearchCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex items-center gap-10 border p-4">
+        <div class="w-[150px]">
+            <ImageNotFound />
+        </div>
+        <div class="flex-1 flex flex-col gap-2">
+            <h4 class="text-xl font-bold">Iphone 13 Pro Azul</h4>
+            <span>Apple</span>
+
+            <div class="flex items-center gap-2">
+                <span>256gb</span>
+                <span>512gb</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
